2cafd637db03054e406793428a7af2b00b585ebd
tushikun <tushikun081376@163.com>
2018-01-29 10:55:48 +0800

    c

--- demo-framework-provider/src/main/java/demos/Test.java
+++ demo-framework-provider/src/main/java/demos/Test.java
@@ -1,10 +1,13 @@
 package demos;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.net.URLDecoder;
 import java.net.URLEncoder;
 
 
 public class Test {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, ClassNotFoundException {
 //		Long a = new Long(null);
 //		System.out.println(a);
 //		String a = "pr2017092514133323121968789-pr2017092209180121775586297-pr2017092514199999121968789";
@@ -13,9 +16,12 @@ public class Test {
 //		System.out.println(b[1]);
 //		System.out.println(b[2]);
 		
-		String a = "订单";
+	/*	String a = "订单";
 		String b = URLEncoder.encode(a);
 		System.out.println(b);
-		System.out.println(URLDecoder.decode(a));
+		System.out.println(URLDecoder.decode(a));*/
+		
 	}
+	
+	
 }
